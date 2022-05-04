bf3a21d12f6f563d837aaff6e29c5d60b72b81b5
310552017 <93580439+310552017@users.noreply.github.com>
2022-05-04 23:04:40 +0800

    test_4(red)

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,8 +1,8 @@
 // game.spec.ts
-test('test on one spare', () => {
-  game.roll(5);
-  game.roll(5); // spare
+test('test on one strike', () => {
+  game.roll(10);
   game.roll(3);
+  game.roll(4);
   rollMany(17, 0);
-  expect(game.score).toBe(16);
+  expect(game.score).toBe(24);
 });
