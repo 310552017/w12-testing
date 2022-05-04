1b56da43d3c0eb6904c0e545250d6f4eb7801d1d
310552017 <93580439+310552017@users.noreply.github.com>
2022-05-04 22:58:09 +0800

    Add files via upload
    reconstruct_1

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,8 +1,30 @@
 // game.spec.ts
-test('all one', () => {
-  const game = new Game();
-  for (let i = 0; i < 20; i++) {
-    game.roll(1);
+import { Game } from './game';
+
+describe('Game', () => {
+  let game: Game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it('should create an instance', () => {
+    expect(game).toBeTruthy();
+  });
+
+  test('all zero', () => {
+    rollMany(20, 0);
+    expect(game.score).toBe(0);
+  });
+
+  test('all one', () => {
+    rollMany(20, 1);
+    expect(game.score).toBe(20);
+  });
+
+  function rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
+    }
   }
-  expect(game.score).toBe(20);
 });
