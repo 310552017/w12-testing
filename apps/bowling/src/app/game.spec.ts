ed24cd6145fb58f55f4942a1fc02fe3ce36bb891
310552017 <93580439+310552017@users.noreply.github.com>
2022-05-04 23:07:26 +0800

    test_5

--- apps/bowling/src/app/game.spec.ts
+++ apps/bowling/src/app/game.spec.ts
@@ -1,8 +1,5 @@
 // game.spec.ts
-test('test on one strike', () => {
-  game.roll(10);
-  game.roll(3);
-  game.roll(4);
-  rollMany(17, 0);
-  expect(game.score).toBe(24);
+test('perfect game', () => {
+  rollMany(12, 10);
+  expect(game.score).toBe(300);
 });
